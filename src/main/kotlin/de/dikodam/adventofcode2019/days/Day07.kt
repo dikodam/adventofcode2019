4a84212e270dd4139e25273240a6e6d2e972e021
Adam <dikodam@googlemail.com>
2019-12-18 12:51:22 +0100

    day 7.2 attempted, still broken

--- src/main/kotlin/de/dikodam/adventofcode2019/days/Day07.kt
+++ src/main/kotlin/de/dikodam/adventofcode2019/days/Day07.kt
@@ -1,36 +1,68 @@
 package de.dikodam.adventofcode2019.days
 
 import de.dikodam.adventofcode2019.utils.IntcodeComputer
+import de.dikodam.adventofcode2019.utils.permutate
 import de.dikodam.adventofcode2019.utils.toIntCode
 
 fun main() {
 
+
     val intCode = day07input.toIntCode()
 
-// first input:    phase setting
-// second input:   program input
-// initial input: 0
-// phase settings: permutations of [0,1,2,3,4]
+    // Task 1
+    val maxThrustSignal1 = (0..4).toSet()
+        .permutate()
+        .map { phaseSetting -> runAmplifiers(intCode, phaseSetting) }
+        .max()!!
 
+    println("Task 1: Phase setting for maximal thrust signal is $maxThrustSignal1")
 
+    // Task 2
+    val maxThrustSignal2 = (5..9).toSet()
+        .permutate()
+        .map { phaseSetting -> runAmplifiers(intCode, phaseSetting) }
+        .max()!!
 
-    // find phase setting that produces maximal thrusterOutput
+    println("Task 2: Phase setting for maximal thrust signal is $maxThrustSignal2")
 
 }
 
-fun runAmplifierSequence(intCode:IntArray, phaseSequence: List<Int>): Int {
+fun runAmplifiers(intCode: IntArray, phaseSequence: List<Int>): Int {
     val ampA = IntcodeComputer(intCode)
     val ampB = IntcodeComputer(intCode)
     val ampC = IntcodeComputer(intCode)
     val ampD = IntcodeComputer(intCode)
     val ampE = IntcodeComputer(intCode)
 
-    val bInput = ampA.run(listOf(phaseSequence[0], 0)).last()
-    val cInput = ampB.run(listOf(phaseSequence[1], bInput)).last()
-    val dInput = ampC.run(listOf(phaseSequence[2], cInput)).last()
-    val eInput = ampD.run(listOf(phaseSequence[3], dInput)).last()
-    return ampE.run(listOf(phaseSequence[4], eInput)).last()
+    val aInput = mutableListOf(phaseSequence[0], 0)
+    val bInput = mutableListOf(phaseSequence[1])
+    val cInput = mutableListOf(phaseSequence[2])
+    val dInput = mutableListOf(phaseSequence[3])
+    val eInput = mutableListOf(phaseSequence[4])
+
+    println("ampA is ${ampA.hashCode()}")
+    println("ampB is ${ampB.hashCode()}")
+    println("ampC is ${ampC.hashCode()}")
+    println("ampD is ${ampD.hashCode()}")
+    println("ampE is ${ampE.hashCode()}")
+
+    Thread().run { ampA.run(input = aInput, output = bInput) }
+    Thread().run { ampB.run(input = bInput, output = cInput) }
+    Thread().run { ampC.run(input = cInput, output = dInput) }
+    Thread().run { ampD.run(input = dInput, output = eInput) }
+    Thread().run { ampE.run(input = eInput, output = aInput) }
+
+    return aInput.last()
 }
+
 
 const val day07input =
     """3,8,1001,8,10,8,105,1,0,0,21,34,55,68,85,106,187,268,349,430,99999,3,9,1001,9,5,9,1002,9,5,9,4,9,99,3,9,1002,9,2,9,1001,9,2,9,1002,9,5,9,1001,9,2,9,4,9,99,3,9,101,3,9,9,102,3,9,9,4,9,99,3,9,1002,9,5,9,101,3,9,9,102,5,9,9,4,9,99,3,9,1002,9,4,9,1001,9,2,9,102,3,9,9,101,3,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,99"""
+
+// max signal: 43210
+const val testInput1 = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
+// max singal: 54321
+const val testInput2 = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+// max signal: 65210
+const val testInput3 =
+    "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
